3a814dc1b82d23d6aab2b23c95632f5a1ffe89bc
Juan Lanuza <juan.lanuza3@gmail.com>
2018-05-16 04:48:16 -0300

    creo input generator de python y empiezo a arreglar la entrada del main

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,11 @@
 
 using namespace std;
 
-int method; //0: kNN, 1: PCA + kNN
-string train_set_path;
-string test_set_path;
-string clasif_path;
+int method = -1; //0: kNN, 1: PCA + kNN
+string train_set_path = "";
+string test_set_path = "";
+string clasif_path = "";
+int knn_k;
 
 int main(int argc, char *argv[]){
 	// ./main -m 1 -i train.csv -q test.csv -o result.csv
@@ -31,31 +32,47 @@ int main(int argc, char *argv[]){
 			test_set_path = argv[i+1];
 		} else if(strcmp(argv[i], "-o") == 0) {
 			clasif_path = argv[i+1];
+		} else if(strcmp(argv[i], "-knn-k") == 0) {
+			knn_k = atoi(argv[i+1]);
+		} else if(strcmp(argv[i], "--help") == 0) {
+			cout << "Parametros para correr el comando ./main :" << endl;
+			cout << "-m  Method (0: knn solo)" << endl;
+			cout << "-i  Path al archivo con el train_set" << endl;
+			cout << "-q  Path al archivo con el test_set" << endl;
+			cout << "-o  Path del archivo de salida con la clasificacion de los datos de test_set" << endl;
+
+			return 0;
 		}
 	}
+	/********* VALIDACIONES DE LOS INPUT *********/
+	if (method == -1) {
+		cout << "Error, se debe pasar el parametro -m con el metodo a utilizar" << endl;
+		return 1;
+	}
+	if (method == 0 && (train_set_path.size() == 0 || test_set_path.size() == 0)) {
+		cout << "Para el metodo knn se debe pasar tanto archivo de train como de test" << endl;
+		return -1;
+	}
 
-	/********* LEO LOS ARCHIVOS PASADOS POR INPUT Y LOS CARGO EN MAPS *********/
+	/********* LEO ARCHIVO PASADO POR INPUT Y LO CARGO EN MAP *********/
 	unordered_map<string, unsigned int> train_set = dataset_file_to_map(train_set_path);
-	unordered_map<string, unsigned int> test_set = dataset_file_to_map(test_set_path);
-
 	/********* PASO IMAGENES DE ENTRENAMIENTO A UNA MATRIZ Y RESULTADOS A UN VECTOR *********/
 	Matrix train_matriz;
 	vector<int> train_clasif;
 	data_map_split(train_set, train_matriz, train_clasif);
 
+	/********* LEO ARCHIVO PASADO POR INPUT Y LO CARGO EN MAP *********/
+	unordered_map<string, unsigned int> test_set = dataset_file_to_map(test_set_path);
 	/********* PASO IMAGENES DE TEST A UNA MATRIZ Y RESULTADOS A UN VECTOR *********/
 	Matrix test_matriz;
 	vector<int> test_clasif;
 	data_map_split(test_set, test_matriz, test_clasif);
 
-	/********* CORRO KNN *********/
-	for (int i = 1; i < 30; i++) {
-		// cout << "Empezando ENTRENAMIENTO para k=" << i << endl;
-		KNN knn(test_matriz, test_clasif, i); // Aca entrena
-		// cout << "ENTRENAMIENTO finalizado" << endl;
-		
-		cout << "K=" << i <<" Score: " << knn.score(test_matriz, test_clasif).accuracy() << endl;
-//		cout << knn.score(test_matriz, test_clasif) << endl;
+
+	if (method == 0) {
+		// KNN solo
+		KNN knn(train_matriz, train_clasif, knn_k); // Aca entrena
+		cout << "K=" << knn_k <<" Score: " << knn.score(test_matriz, test_clasif).accuracy() << endl;
 	}
 
 	return 0;
---
+++ tests/input_generator.py
@@ -0,0 +1,56 @@
+from os import listdir
+from os.path import isfile, join, isdir
+import pandas as pd
+from math import floor
+
+
+def listdir_paths(path):
+    return [join(path, fil) for fil in listdir(path)]
+
+
+def dataset_to_dict(dataset_path):
+    data = {}
+    for person_file in listdir(dataset_path):
+        person_path = join(dataset_path, person_file)
+        if isdir(person_path):
+            data[person_file] = []
+            for img_file in listdir(person_path):
+                if isfile(join(person_path, img_file)):
+                    data[person_file].append(join(person_path, img_file))
+    return data
+
+
+def dataset_to_df(dataset_path):
+    return pd.DataFrame(dataset_to_dict(dataset_path))
+
+
+def split_dataset(dataset_path, frac):
+    df = dataset_to_df(dataset_path)
+    total = len(df.index)
+    train = floor(total * frac)
+    test = total - train
+    return (df[0:train], df[-test:])
+
+
+def colname_to_int(col):
+    return ''.join(c for c in col if c.isdigit())
+
+
+def traindf_to_csv(df, path):
+    with open(path, 'w') as archivo:
+        for key, col in df.iteritems():
+            for index, elem in col.iteritems():
+                archivo.write(elem + ', ' + colname_to_int(key) + ',\n')
+
+
+def testdf_to_csv(df, path):
+    with open(path, 'w') as archivo:
+        for key, col in df.iteritems():
+            for index, elem in col.iteritems():
+                archivo.write(elem + ',\n')
+
+
+# Main
+(train_df, test_df) = split_dataset('../assets/ImagenesCaras', 0.8)
+traindf_to_csv(train_df, 'nuestros/ImagenesCaras80.train')
+testdf_to_csv(test_df, 'nuestros/ImagenesCaras80.test')
