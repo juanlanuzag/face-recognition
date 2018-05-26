be7a2c2a1ac14195f0dfd4db895184ac7ecf49a4
Juan Lanuza <juan.lanuza3@gmail.com>
2018-05-25 21:42:24 -0300

    acomodando archivos

--- tests/experimento1.sh
+++
@@ -1,4 +0,0 @@
-for i in $(seq 1 80); do
-    echo Corriendo para k=$i;
-    ./main -m 0 -i ../tests/nuestros/ImagenesCaras80.train -q ../tests/nuestros/ImagenesCaras80.test -o ../tests/nuestros/knnsolo$i -knn-k $i;
-done
